4ce53dbddae826a2f73419aa43bdb4cc28ac9054
Savyez <visnuk252@gmail.com>
2026-02-03 22:29:14 +0530

    fix: unwrap express app for netlify functions

--- netlify/functions/express.js
+++ netlify/functions/express.js
@@ -1,4 +1,6 @@
 import serverless from 'serverless-http';
 import app from '../../app.js';
 
-export const handler = serverless(app);
+const expressApp = app && app.default ? app.default : app;
+
+export const handler = serverless(expressApp);
