518f5eee579dbc20844b37d13fa22ed9125170b6
Savyez <visnuk252@gmail.com>
2026-02-03 22:31:39 +0530

    fix: include views and public for netlify functions

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 import express from 'express';
 import axios from 'axios';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config({
     path: '.env'
 });
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Axios configuration with API key from environment variables
 const axiosConfig = {
